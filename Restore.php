746bf1a63f0e750d6eb670755f28e71d52f7de77
Andrew Nagy <andrew.nagy@the159.com>
2019-02-20 16:58:29 -0500

    fix legacy restore

--- Restore.php
+++ Restore.php
@@ -2,32 +2,28 @@
 namespace FreePBX\modules\Arimanager;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    $this->FreePBX->Arimanager->LoadUsers($configs['users']);
-    if(is_array($configs['advanced'])){
-      foreach ($configs['advanced'] as $key => $value) {
-        $this->FreePBX->Config->update($key,$value);
-      }
-    }
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $tables = array_flip($tables + $unknownTables);
-    if (!isset($tables['arimanager'])) {
-      return $this;
-    }
-    $arim = $this->FreePBX->Arimanager;
-    $arim->setDatabase($pdo);
-    $users = $arim->getAllUsers();
-    $arim->resetDatabase();
-    $arim->LoadUsers($users);
-    $ampconf = $this->getAMPConf($pdo);
-    $advanced = ['ENABLE_ARI', 'FPBX_ARI_USER', 'FPBX_ARI_PASSWORD', 'ENABLE_ARI_PP', 'ARI_WS_WRITE_TIMEOUT', 'ARI_ALLOWED_ORIGINS'];
-    foreach ($advanced as $key) {
-      if(isset($ampconf[$key])){
-        $this->FreePBX->Config->update($key, $ampconf[$key]);
-      }
-    }
-    return $this;
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$this->FreePBX->Arimanager->LoadUsers($configs['users']);
+		if(is_array($configs['advanced'])){
+			foreach ($configs['advanced'] as $key => $value) {
+				$this->FreePBX->Config->update($key,$value);
+			}
+		}
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$advanced = ['ENABLE_ARI', 'FPBX_ARI_USER', 'FPBX_ARI_PASSWORD', 'ENABLE_ARI_PP', 'ARI_WS_WRITE_TIMEOUT', 'ARI_ALLOWED_ORIGINS'];
+		foreach ($advanced as $key) {
+			if(isset($data['settings'][$key])){
+				$this->FreePBX->Config->update($key, $data['settings'][$key]);
+			}
+		}
+
+		$tables = ['arimanager'];
+		foreach($tables as $table) {
+			$sth = $pdo->query("SELECT * FROM $table",\PDO::FETCH_ASSOC);
+			$res = $sth->fetchAll();
+			$this->addDataToTableFromArray($table, $res);
+		}
+	}
 }
