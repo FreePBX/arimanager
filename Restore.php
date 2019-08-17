7a80e83dc144e3dce1d2619ce4d139e1571129d8
nthumann <me@n-thumann.de>
2019-08-17 21:09:09 +0000

    Related to FREEPBX-20414: removed $jobid from runRestore()

--- Restore.php
+++ Restore.php
@@ -2,7 +2,7 @@
 namespace FreePBX\modules\Arimanager;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
+	public function runRestore(){
 		$configs = $this->getConfigs();
 		$this->FreePBX->Arimanager->LoadUsers($configs['users']);
 		if(is_array($configs['advanced'])){
