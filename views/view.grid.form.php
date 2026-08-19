2a83cc64606667a23ab1cd5f5a3829d980871de9
Philip Joseph <jphilip@sangoma.com>
2026-08-19 17:05:55 +0530

    v18 changes

--- views/view.grid.form.php
+++ views/view.grid.form.php
@@ -7,6 +7,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
+                
                 <button type="button" class="close" data-bs-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title"><?php echo _("Loading...") ?></h4>
             </div>
